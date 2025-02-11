c58a0ec1095e7a8187603b0017e90ebc9996a35a
Lowmst <im.liuhongyuan@outlook.com>
2025-02-11 18:47:39 +0800

    添加项目文件。

---
+++ SharpWrapper/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System.Runtime.InteropServices;
+
+namespace SharpWrapper
+{
+    public class Program()
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("你好");
+            Audio2WAV.libmain(args[0]);
+        }
+    }
+
+    partial class Audio2WAV
+    {
+        [LibraryImport("LibAudio2WAV.dll", StringMarshalling = StringMarshalling.Utf8)]
+        public static partial void libmain(string filepath);
+    }
+}
